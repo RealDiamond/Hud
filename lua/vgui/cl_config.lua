ac5ec0bbd4e3a5f8725efba66e69c48af02126b3
Dev:Rudy <31436188+RealDiamond@users.noreply.github.com>
2017-11-09 20:38:26 +0300

    Update cl_config.lua

--- lua/vgui/cl_config.lua
+++ lua/vgui/cl_config.lua
@@ -11,38 +11,35 @@ Version: 1.1.0
 --------------------------------------------------------------------------------------------------------
 13:20 03.11.2017 | Добавлен | HUDConfig.
 -------------------------------------------------------------------------------------------------------]]
-
 local config = {}
-
+--============================Font==============================--
 if CLIENT then
     
     surface.CreateFont("ui.40", { font = "Arial", size = 17.800, weight = 2500, outline = false, }) 
     surface.CreateFont("ui.39", { font = "Arial", size = 18, weight = 2500, outline = false, }) 
     surface.CreateFont("ui.38", { font = "Arial", size = 21, weight = 2500, outline = false, }) 
     surface.CreateFont("ui.37", { font = "Arial", size = 18, weight = 2500, outline = false, }) 
-    surface.CreateFont("ui.36", { font = "Arial", size = 17, weight = 2500, outline = false, })     
+    surface.CreateFont("ui.36", { font = "Arial", size = 17, weight = 2500, outline = false, }) 
         
 end      
-
-config.Radius = 0
-
-config.laws = Color( 120,120,120,120 )
-
-config.basecolor = Color( 40,40,40,220 )
-config.Outlined = Color( 255,255,255,255 )
-
-config.health = Color( 255,10,10,220 )
-config.armor = Color( 10,10,255,220 )
-
-config.text = Color( 255,255,255,220 )
-
-config.icon = Color( 255,255,255,255 )
-
+--===========================Config==============================--
+config.Radius    = 0                          -- Radius (0 |max 4)
+config.basecolor = Color( 40,40,40,220 )      -- basecolor     : |40,40,40,220|120,120,120,220|0,0,0,220|
+config.Outlined  = Color( 255,255,255,255 )   -- outlined      : |255,255,255,255|0,0,0,255|
+config.laws      = Color( 120,120,120,120 )   -- Color laws    : |40,40,40,120|120,120,120,120|
+config.health    = Color( 255,10,10,220 )     -- Color healrh  : |255,10,10,220|
+config.armor     = Color( 10,10,255,220 )     -- Color armor   : |10,10,255,220|120,120,120,220|
+config.text      = Color( 255,255,255,220 )   -- Color text    : |255,255,255,220|
+config.icon      = Color( 255,255,255,255 )   -- Color icon    : |255,255,255,255|
+--============================language============================--
 config.lockdown_text = "Мэр объявил комендантский час!"
-config.health_text = "Здоровье"
-config.armor_text = "Броня"
+config.health_text   = "Здоровье"
+config.armor_text    = "Броня"
+config.laws_text     = "*Законы города*"
+--============================End Config============================--
 
---НЕ трогать!--
+--========НЕ ТРОГАТЬ!========--
 function hud_main_config()
-	return config
+    return config
 end 
+-------------------------------
