d92984a6109e686fd9d4f2d4d92dd2b29436e6e1
Rudy <31436188+RealDiamond@users.noreply.github.com>
2017-11-25 17:18:33 +0300

    Update autorun.lua

--- lua/autorun/autorun.lua
+++ lua/autorun/autorun.lua
@@ -1,7 +1,13 @@
 if CLIENT then
+    
     AddCSLuaFile('vgui/cl_main.lua')
     AddCSLuaFile('vgui/cl_config.lua')
-else
+    
+end
+
+if SERVER then
+    
     include('vgui/cl_main.lua.lua')
     include('vgui/cl_config.lua')
+    
 end
